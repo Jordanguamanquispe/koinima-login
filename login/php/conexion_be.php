8caa578925a0ec33c5123ceb1ac393fd1da164e8
jeff_853 <144867971+Jordanguamanquispe@users.noreply.github.com>
2024-06-02 13:17:48 -0500

    Add files via upload

---
+++ login/php/conexion_be.php
@@ -0,0 +1,10 @@
+
+<?php
+    $conexion = mysqli_connect("localhost", "root", "", "login_register");
+    //if ($conexion){
+    //    echo 'conectado exitosamente';
+    //}else{
+
+     //   echo 'vales vrg';
+    //}
+?>
